a903891761ab0ba92e06ff59d1c51a2e35adb93b
Spencer Owens <sowens23@alaska.edu>
2023-09-21 21:42:10 -0800

    20230921

--- homework/assignment2/fsarray.cpp
+++ homework/assignment2/fsarray.cpp
@@ -7,7 +7,7 @@
 	 Program Function: for testing purposes
 */
 
-#include "fsarray.hpp"
+#include "brucefsarray.hpp"
 #include <iostream>
 	using std::cout;
 	using std::endl;
@@ -16,35 +16,30 @@ int main () {
 	FSArray<int> ia(8);           // Array of 8 ints
 	FSArray<int> iax;             // Another array of 8 ints
 	FSArray<double> da(40, 3.2);  // Array of 40 doubles; each is set to 3.2
-	// FSArray x(8);                 // DOES NOT COMPILE; no template parameter
+	//FSArray x(8);                 // DOES NOT COMPILE; no template parameter
 
 	// Set all items (counter loop)
 	for (int c = 0; c < ia.size(); ++c)
 	{
-			ia[c] = c * c;
+		ia[c] = c * c;
 	}
 
 	// Range-based for-loop calls FSArray member functions begin, end
 	for (auto x : ia)
 	{
-			cout << "Item :" << x << endl;
+		cout << "Item :" << x << endl;
 	}
 
 	const FSArray<int> ia2 = ia;  // Copy constructor
 	if (ia2 == ia)                // Condition is true
-			cout << "Equal!" << endl;
+		cout << "Equal!" << endl;
 
-	std::cout << "1\n";
-	//FSArray<double> da2;
-	FSArray<int> ib;
-	std::cout << "2\n";
-	//da2 = da;                     // Copy assignment
-	ia = ib;                     // Copy assignment
-	std::cout << "3\n";
-	// da2 = ia;                     // DOES NOT COMPILE; different value types
+	FSArray<double> da2;
+	da2 = da;                     // Copy assignment
+	//da2 = ia;                     // DOES NOT COMPILE; different value types
 
-	// if (da == ia)                 // DOES NOT COMPILE; different value types
-	// 		cout << "blah blah" << endl;
+	//if (da == ia)                 // DOES NOT COMPILE; different value types
+	//	cout << "blah blah" << endl;
 	
 	return 0;
 }
