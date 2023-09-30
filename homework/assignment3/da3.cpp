123f9c2d02b47018a885f7b4417290b31069d2e3
Spencer Owens <thespencerowens@outlook.com>
2023-09-30 00:50:19 -0800

    20230930: HW03 Finished - Midnight oil again.

--- homework/assignment3/da3.cpp
+++ homework/assignment3/da3.cpp
@@ -9,7 +9,12 @@
 
 #include "da3.hpp"     // For Assignment 3 prototypes & templates
 
-#include<iostream>
+#include <iostream>
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+#include <stdexcept>		// For std::out_of_range
 #include <functional>
 using std::function;
 
@@ -17,31 +22,89 @@ using std::function;
 void didItThrow(const function<void()> & ff,
                 bool & threw)
 {
-    // TODO: WRITE THIS!!!
+		try {
+			ff();
+			threw = false; 
+		} catch (...) {
+			threw = true;
+			throw;
+		}
 }
 
 
 int gcd(int a,
         int b)
 {
-    return 42;  // DUMMY
-    // TODO: WRITE THIS!!!
+	if (a==0) return b; // Return GCD
+	return gcd(b % a, a); // Recursively call gcd after modulo operation
 }
 
-int main() {
-	// TESTING
-	std::cout<<"testing\n";
-	// Filling Node
-	LLNode<int> *node=new
-	LLNode<int>(5);
-	node->_next = new LLNode<int>(10);
-	node->_next->_next = new LLNode<int>(15);
-	// Printing Node
-	LLNode<int> *current = node;
-	while (current != nullptr) {
-			std::cout << current->_data << std::endl;
-			current = current->_next;
-	}
-	delete node;
-	return 0;
+void myFunc() { 
+	throw std::runtime_error("Oh no!"); 
 }
+
+/* int main() {
+	// TESTING NODE INITIALIZATION
+		std::cout<<"Testing initialization\n";
+		LLNode<int> *node=new
+		LLNode<int>(5);
+		node->_next = new LLNode<int>(10);
+		node->_next->_next = new LLNode<int>(15);
+
+	// TESTING NODE PRINT
+		std::cout<< "Testing iterative print\n";
+		LLNode<int> *current = node;
+		while (current != nullptr) {
+				std::cout << current->_data << std::endl;
+				current = current->_next;
+		}
+
+	// TESTING INDEX RETURN
+		try {
+			std::size_t index_to_find = 2;
+			int value_at_index = lookup(node, index_to_find);
+			std::cout<< "Index value of node at 2: " << value_at_index << std::endl;
+		} catch (const std::out_of_range& e) {
+			std::cout << "Error: " << e.what() << std::endl;
+		}
+
+	// Testing didItThrow
+		bool result;
+		try {
+				didItThrow(myFunc, result);
+		}
+		catch (std::exception & e) {
+				if (result)
+						std::cout << "SUCCESS" << std::endl;
+				else
+						std::cout << "FAILURE" << std::endl;
+		}
+
+	// Testing checkSorted
+		vector<int> v {
+				4,
+				3,
+				2,
+				1
+		};
+		result = checkSorted(v.begin(), v.end());
+		if (result) std::cout<<"True\n";
+		else std::cout<<"False\n";
+ 		vector<int> b {
+				1,
+				2,
+				3,
+				4
+		};
+		result = checkSorted(b.begin(), b.end());
+		if (result) std::cout<<"True\n";
+		else std::cout<<"False\n";
+
+	// Testing GCD
+		int gcd_test = gcd(910,42);
+		std::cout << "GCD(910,42): " << gcd_test << std::endl;
+
+	// Delete and return
+		delete node;
+		return 0;
+} */
--- homework/assignment3/da3.hpp
+++ homework/assignment3/da3.hpp
@@ -21,6 +21,7 @@
 
 #include "llnode.hpp"  // For LLNode
 #include <cstddef>     // For std::size_t
+#include <stdexcept>		// For std::out_of_range
 #include <functional>  // For std::function
 
 
@@ -28,13 +29,24 @@ template <typename ValueType>
 ValueType lookup(const LLNode<ValueType> * head,
                  std::size_t index)
 {
-    // return ValueType();  // DUMMY
-    // TODO: WRITE THIS!!!
-		int count=0;
-		current
-		for (int i=0;i<=index;i++) {
-			
+		// Check for out of range
+		if(!head && index != 0) {
+			throw std::out_of_range("Index is out of range for empty list");
 		}
+
+		// Iterate through the list up to the given index
+		const LLNode<ValueType>* current = head;
+		std::size_t currentIndex = 0;
+		while (currentIndex < index && current != nullptr) {
+			current = current->_next;
+			currentIndex++;
+		}
+
+		// Check to see if nullptr was hit before index
+		if (!current) throw std::out_of_range("Index is out of range for list");
+
+		// Index found
+		return current->_data;
 }
 
 
@@ -47,10 +59,22 @@ template <typename FDIter>
 bool checkSorted(FDIter first,
                  FDIter last)
 {
-    return false;  // DUMMY
-    // TODO: WRITE THIS!!!
-}
+	// Invariant: What happens when first and last are equal?
+	if (first == last) return true;
 
+	FDIter next = first; 
+	++next;
+
+	while(next != last) {
+		if (*next < *first) {
+			return false;
+		}
+		++first;
+		++next;
+	}
+	// Returning true if all next values are 
+	return true;
+}
 
 // Implementation in source file
 int gcd(int a,
