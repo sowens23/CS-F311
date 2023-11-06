858e61a50412792ed1027146fde94e66f220c9fe
Spencer Owens <sowens23@alaska.edu>
2023-11-06 12:46:11 -0900

    20231106: Start da6

---
+++ homework/assignment6/da6.hpp
@@ -0,0 +1,27 @@
+/* 
+Name: Spencer Owens
+Class: 23F CS-311 Data Structures and Algorithms
+Assignment: TheMarvouslySmartArray (Assignment 5)
+Created: 2023-Oct-28
+Updated: 2023-Nov-02
+File Name: tsmarray.hpp
+Current Version: 7
+File Function: 
+    To serve as an array of a client-defined type with all of the generic container functionality
+    So it can interact with the STL library functions. 
+
+Version History:
+- v7:
+    - Updated array to template with user defined type
+    - Wrote operators, copy, const copy, move
+    - Wrote functions resize, insert, swap, and erase
+    - Wrote in function guarantees, preconditions, and asserts
+    - Program successfully runs using tmsarray_test.hpp 
+        - This demonstrates that the code written is written well enough to serve as a generic container.
+*/
+
+#include <iostream>
+
+int main() {
+  return 0;
+}
